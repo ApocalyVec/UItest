9b220fece88a9291bb8bae8bf3fc738e75d7f62b
Ziheng (Leo) Li <s-vector.lee@hotmail.com>
2018-11-01 21:19:36 -0400

    Initial commit

---
+++ app/src/main/java/com/ece2799_b18_team_9/uitest/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ece2799_b18_team_9.uitest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
